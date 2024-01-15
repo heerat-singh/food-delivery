a0ed1fd3aafb82b3e78b432a69f0dd80d7fb0362
heerat-singh <heeratsingh54@gmail.com>
2024-01-15 00:01:33 -0500

    first sommit

---
+++ foodcatalogue/src/main/java/com/heeratsingh/foodcatalogue/mapper/FoodItemMapper.java
@@ -0,0 +1,18 @@
+package com.heeratsingh.foodcatalogue.mapper;
+
+import com.heeratsingh.foodcatalogue.dto.FoodItemDTO;
+import com.heeratsingh.foodcatalogue.entity.FoodItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface FoodItemMapper {
+
+    FoodItemMapper INSTANCE = Mappers.getMapper(FoodItemMapper.class);
+
+    FoodItem mapFoodItemDTOToFoodItem(FoodItemDTO foodItemDTO);
+
+    FoodItemDTO mapFoodItemToFoodItemDto(FoodItem foodItem);
+
+
+}
---
+++ foodcatalogue/src/main/java/com/heeratsingh/foodcatalogue/service/FoodCatalogueService.java
@@ -0,0 +1,52 @@
+package com.heeratsingh.foodcatalogue.service;
+
+import com.heeratsingh.foodcatalogue.dto.FoodItemDTO;
+import com.heeratsingh.foodcatalogue.dto.FoodCataloguePage;
+import com.heeratsingh.foodcatalogue.dto.Restaurant;
+import com.heeratsingh.foodcatalogue.entity.FoodItem;
+import com.heeratsingh.foodcatalogue.mapper.FoodItemMapper;
+import com.heeratsingh.foodcatalogue.repo.FoodItemRepo;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class FoodCatalogueService {
+
+    @Autowired
+    FoodItemRepo foodItemRepo;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+
+    public FoodItemDTO addFoodItem(FoodItemDTO foodItemDTO) {
+        FoodItem foodItemSavedInDB = foodItemRepo.save(FoodItemMapper.INSTANCE.mapFoodItemDTOToFoodItem(foodItemDTO));
+        return FoodItemMapper.INSTANCE.mapFoodItemToFoodItemDto(foodItemSavedInDB);
+    }
+
+    public FoodCataloguePage fetchFoodCataloguePageDetails(Integer restaurantId) {
+        List<FoodItem> foodItemList =  fetchFoodItemList(restaurantId);
+        Restaurant restaurant = fetchRestaurantDetailsFromRestaurantMS(restaurantId);
+        return createFoodCataloguePage(foodItemList, restaurant);
+    }
+
+    private FoodCataloguePage createFoodCataloguePage(List<FoodItem> foodItemList, Restaurant restaurant) {
+        FoodCataloguePage foodCataloguePage = new FoodCataloguePage();
+        foodCataloguePage.setFoodItemsList(foodItemList);
+        foodCataloguePage.setRestaurant(restaurant);
+        return foodCataloguePage;
+    }
+
+    private Restaurant fetchRestaurantDetailsFromRestaurantMS(Integer restaurantId) {
+        return restTemplate.getForObject("http://RESTAURANT-SERVICE:9091/restaurant/fetchById/"+restaurantId, Restaurant.class);
+    }
+
+    private List<FoodItem> fetchFoodItemList(Integer restaurantId) {
+        return foodItemRepo.findByRestaurantId(restaurantId);
+    }
+}
